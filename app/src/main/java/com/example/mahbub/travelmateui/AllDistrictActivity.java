91cd14b3429857d885be75601cdb4d433ab68abc
rmahbub63 <rmahbub63@gmail.com>
2017-11-23 00:43:32 +0600

    previous problem solve and fine tuning

--- app/src/main/java/com/example/mahbub/travelmateui/AllDistrictActivity.java
+++ app/src/main/java/com/example/mahbub/travelmateui/AllDistrictActivity.java
@@ -19,14 +19,14 @@ public class AllDistrictActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_district);
 
         divisions = new ArrayList<>();
-        divisions.add(new DivisionModel(R.drawable.dhaka_division, "Dhaka"));
+        divisions.add(new DivisionModel(R.drawable.barisal_div_image, "Barisal"));
         divisions.add(new DivisionModel(R.drawable.chittagong_division, "Chittagong"));
-        divisions.add(new DivisionModel(R.drawable.rajshahi_division, "Rajshahi"));
+        divisions.add(new DivisionModel(R.drawable.dhaka_division, "Dhaka"));
         divisions.add(new DivisionModel(R.drawable.khulna_division, "Khulna"));
-        divisions.add(new DivisionModel(R.drawable.chandpur_boro_stattion, "Barishal"));
-        divisions.add(new DivisionModel(R.drawable.dhaka_division, "Sylhet"));
-        divisions.add(new DivisionModel(R.drawable.dhaka_division, "Rangpur"));
         divisions.add(new DivisionModel(R.drawable.mymensingh_division, "Mymensingh"));
+        divisions.add(new DivisionModel(R.drawable.rajshahi_division, "Rajshahi"));
+        divisions.add(new DivisionModel(R.drawable.rangpur_division, "Rangpur"));
+        divisions.add(new DivisionModel(R.drawable.sylhet_div_image, "Sylhet"));
 
         RecyclerView divisionList = findViewById(R.id.vertical_recycler_view);
         divisionList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
