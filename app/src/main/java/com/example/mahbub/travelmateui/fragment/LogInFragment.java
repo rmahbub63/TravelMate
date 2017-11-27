6c3dc4673bb0fd469719f0a9ef5d8eee2ba88b4c
shajjad29 <shajjadhowlader@gmail.com>
2017-11-28 02:17:34 +0600

    fb login include

--- app/src/main/java/com/example/mahbub/travelmateui/fragment/LogInFragment.java
+++ app/src/main/java/com/example/mahbub/travelmateui/fragment/LogInFragment.java
@@ -7,17 +7,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mahbub.travelmateui.MainOptionSelectActivity;
 import com.example.mahbub.travelmateui.R;
 import com.example.mahbub.travelmateui.model.LoginModel;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 /**
  * Created by SHAJJAD on 27-Nov-17.
  */
 
 public class LogInFragment extends  Fragment {
-
+    LoginButton login_button;
+    CallbackManager callbackManager;
     private Button buttonLogin;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle){
@@ -34,7 +41,36 @@ public class LogInFragment extends  Fragment {
                 startActivity(intent);
             }
         });
+        //facebook login
+        login_button=mainView.findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
 
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(getContext(),"Success",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getContext(),MainOptionSelectActivity.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
         return mainView;
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
--- app/src/main/java/com/example/mahbub/travelmateui/fragment/SignUpFragment.java
+++ app/src/main/java/com/example/mahbub/travelmateui/fragment/SignUpFragment.java
@@ -1,20 +1,49 @@
 package com.example.mahbub.travelmateui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.mahbub.travelmateui.LoginRegistrationActivity;
+import com.example.mahbub.travelmateui.MainOptionSelectActivity;
 import com.example.mahbub.travelmateui.R;
+import com.example.mahbub.travelmateui.model.LoginModel;
 
 /**
  * Created by SHAJJAD on 27-Nov-17.
  */
 
 public class SignUpFragment extends Fragment {
+    private Button buttonSignup,buttonGuest;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle){
-        return inflater.inflate(R.layout.activity_signup,null);
+        View mainView = inflater.inflate(R.layout.activity_signup,null);
+
+
+        buttonSignup = mainView.findViewById(R.id.buttonSignup);
+        buttonGuest = mainView.findViewById(R.id.buttonGuest);
+        //SignUp to login activity
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), LoginRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+          //Guest entry
+        buttonGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MainOptionSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return mainView;
     }
 }
