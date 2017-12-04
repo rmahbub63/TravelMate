000eebefb2e2d864c7f120bcbadb2ecf3214ec54
shajjad29 <shajjadhowlader@gmail.com>
2017-12-04 14:00:15 +0600

    xml of MyTourPlanNext

---
+++ app/src/main/java/com/example/mahbub/travelmateui/MyTourPlanNext.java
@@ -0,0 +1,13 @@
+package com.example.mahbub.travelmateui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTourPlanNext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tour_plan_next);
+    }
+}
